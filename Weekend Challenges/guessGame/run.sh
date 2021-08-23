f95acaf51298f8e44b995527943b917dd6f3d017
emanue1e <80365291+emanue1e@users.noreply.github.com>
2021-08-23 18:27:36 +0930

    Delete Weekend Challenges/guessGame directory

--- Weekend Challenges/guessGame/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-figlet "Welcome to the Guessing Game!" -f smslant -c
-
-bash guess.sh
-if [ $cpuNum -eq $guess ]
-then
-	figlet "Welcome to the C Guessing Game!" -f smslant -c
-	./a.out
-fi
